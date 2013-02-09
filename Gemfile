e9759f29399e7732a43eacb7ee6bf0b01d3625e1
Timur Kozmenko <timraell@gmail.com>
2013-02-09 18:38:38 +1100

    Initialize rails app

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'pg'
+
+group :assets do
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'debugger'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
+  gem 'database_cleaner'
+  gem 'webmock'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Client::Application.config.secret_token = 'c1ffd740affccab5045151a0d128f4e42cfc054c9c09d5aaeb74d9d6e9110ae69d412da541f27be48837fa9a6811b0d737b88ca38cc45f998cdccbd6df380665'
